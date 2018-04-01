f52eeb6f9d7fb002b7f160619e9bb56a86351242
Peter Law <PeterJCLaw@gmail.com>
2018-04-01 21:18:57 +0100

    Implement scoring logic

--- scoring/score.py
+++ scoring/score.py
@@ -1,5 +1,15 @@
+import collections
+import itertools
+
+
 class InvalidScoresheetException(Exception):
     pass
+
+
+TOKEN_HOME_CORNERS = 'PGYO'
+EXPECTED_TOKEN_COUNTS = {x: 5 for x in TOKEN_HOME_CORNERS}
+
+ZONES = list(range(len(TOKEN_HOME_CORNERS))) + ['other']
 
 
 class Scorer(object):
@@ -7,8 +17,43 @@ class Scorer(object):
         self._teams_data = teams_data
         self._arena_data = arena_data
 
+    def _score_team(self, team_info):
+        zone_id = team_info['zone']
+        own_token = TOKEN_HOME_CORNERS[zone_id]
+
+        token_string = self._arena_data[zone_id]['tokens'].replace(' ', '')
+
+        score = 1 if team_info.get('moved') else 0
+
+        for token in token_string:
+            if token == own_token:
+                score += 2
+            else:
+                score -= 1
+
+        return score
+
     def calculate_scores(self):
-        raise NotImplementedError()
+        return {
+            team_id: self._score_team(team_info)
+            for team_id, team_info in self._teams_data.items()
+        }
+
+    def validate(self, extra_data):
+        all_tokens = ''.join(
+            self._arena_data[zone_id]['tokens']
+            for zone_id in ZONES
+        )
+
+        token_counts = collections.Counter(all_tokens.replace(' ', ''))
+
+        if token_counts != EXPECTED_TOKEN_COUNTS:
+            raise InvalidScoresheetException(
+                "Wrong token counts. Should be five of each of '{}' but was {!r}".format(
+                    "', '".join(EXPECTED_TOKEN_COUNTS.keys()),
+                    token_counts,
+                ),
+            )
 
 
 if __name__ == '__main__':
--- scoring/tests/test_scoring.py
+++ scoring/tests/test_scoring.py
@@ -11,5 +11,252 @@ sys.path.insert(0, ROOT)
 from score import Scorer, InvalidScoresheetException
 
 
+class TestScorer(unittest.TestCase):
+    longMessage = True
+
+    def assertScores(self, expected, teams_data, arena_data):
+        scorer = Scorer(teams_data, arena_data)
+        scores = scorer.calculate_scores()
+
+        self.assertEqual(expected, scores, "Wrong scores!")
+
+    def assertValidationError(self, teams_data, arena_data):
+        with self.assertRaises(InvalidScoresheetException):
+            scorer = Scorer(teams_data, arena_data)
+            scores = scorer.validate(None)
+
+    def test_scores_match_start(self):
+        teams_data = {
+            'ABC': {'zone': 0},
+            'DEF': {'zone': 1},
+            'GHI': {'zone': 2},
+        }
+        arena_data = {
+            0: { 'tokens': 'YYYYY' },
+            1: { 'tokens': 'OOOOO' },
+            2: { 'tokens': 'PPPPP' },
+            3: { 'tokens': 'GGGGG' },
+            'other': { 'tokens': '' },
+        }
+        expected = {
+            'ABC': -5,
+            'DEF': -5,
+            'GHI': -5,
+        }
+
+        self.assertScores(expected, teams_data, arena_data)
+
+    def test_scores_match_zeros(self):
+        teams_data = {
+            'ABC': {'zone': 0},
+            'DEF': {'zone': 1},
+            'GHI': {'zone': 2},
+        }
+        arena_data = {
+            0: { 'tokens': '' },
+            1: { 'tokens': '' },
+            2: { 'tokens': '' },
+            3: { 'tokens': '' },
+            'other': { 'tokens': 'PGOY PGOY PGOY PGOY PGOY' },
+        }
+        expected = {
+            'ABC': 0,
+            'DEF': 0,
+            'GHI': 0,
+        }
+
+        self.assertScores(expected, teams_data, arena_data)
+
+    def test_scores_zone_with_mixed_opponent_colours(self):
+        teams_data = {
+            'ABC': {'zone': 0},
+            'DEF': {'zone': 1},
+            'GHI': {'zone': 2},
+        }
+        arena_data = {
+            0: { 'tokens': 'GOY GOY' },
+            1: { 'tokens': '' },
+            2: { 'tokens': '' },
+            3: { 'tokens': '' },
+            'other': { 'tokens': 'P P PGOY PGOY PGOY' },
+        }
+        expected = {
+            'ABC': -6,
+            'DEF': 0,
+            'GHI': 0,
+        }
+
+        self.assertScores(expected, teams_data, arena_data)
+
+    def test_scores_zone_with_own_colours(self):
+        teams_data = {
+            'ABC': {'zone': 0},
+            'DEF': {'zone': 1},
+            'GHI': {'zone': 2},
+        }
+        arena_data = {
+            0: { 'tokens': 'PPP' },
+            1: { 'tokens': '' },
+            2: { 'tokens': '' },
+            3: { 'tokens': '' },
+            'other': { 'tokens': 'GOY GOY GOY PGOY PGOY' },
+        }
+        expected = {
+            'ABC': 6,
+            'DEF': 0,
+            'GHI': 0,
+        }
+
+        self.assertScores(expected, teams_data, arena_data)
+
+    def test_scores_zone_with_mixed_colours(self):
+        teams_data = {
+            'ABC': {'zone': 0},
+            'DEF': {'zone': 1},
+            'GHI': {'zone': 2},
+        }
+        arena_data = {
+            0: { 'tokens': 'PPP GG OO' },
+            1: { 'tokens': '' },
+            2: { 'tokens': '' },
+            3: { 'tokens': '' },
+            'other': { 'tokens': 'Y Y GOY PGOY PGOY' },
+        }
+        expected = {
+            'ABC': 2,
+            'DEF': 0,
+            'GHI': 0,
+        }
+
+        self.assertScores(expected, teams_data, arena_data)
+
+    def test_scores_zones_with_mixed_colours(self):
+        teams_data = {
+            'ABC': {'zone': 0},
+            'DEF': {'zone': 1},
+            'GHI': {'zone': 2},
+        }
+        arena_data = {
+            0: { 'tokens': 'PGOY PP' },
+            1: { 'tokens': 'GPO G' },
+            2: { 'tokens': 'YYY' },
+            3: { 'tokens': 'OY' },
+            'other': { 'tokens': 'P OO GG' },
+        }
+        expected = {
+            'ABC': 3,
+            'DEF': 2,
+            'GHI': 6,
+        }
+
+        self.assertScores(expected, teams_data, arena_data)
+
+    def test_scores_movement_point(self):
+        teams_data = {
+            'ABC': {'zone': 0, 'moved': True},
+            'DEF': {'zone': 1, 'moved': False},
+            'GHI': {'zone': 2, 'moved': False},
+        }
+        arena_data = {
+            0: { 'tokens': '' },
+            1: { 'tokens': '' },
+            2: { 'tokens': '' },
+            3: { 'tokens': '' },
+            'other': { 'tokens': 'PGOY PGOY PGOY PGOY PGOY' },
+        }
+        expected = {
+            'ABC': 1,
+            'DEF': 0,
+            'GHI': 0,
+        }
+
+        self.assertScores(expected, teams_data, arena_data)
+
+    def test_scores_movement_point_and_tokens(self):
+        teams_data = {
+            'ABC': {'zone': 0, 'moved': True},
+            'DEF': {'zone': 1, 'moved': False},
+            'GHI': {'zone': 2, 'moved': False},
+        }
+        arena_data = {
+            0: { 'tokens': 'PGOY PP' },
+            1: { 'tokens': 'GPO G' },
+            2: { 'tokens': 'YYY' },
+            3: { 'tokens': 'OY' },
+            'other': { 'tokens': 'P OO GG' },
+        }
+        expected = {
+            'ABC': 4,
+            'DEF': 2,
+            'GHI': 6,
+        }
+
+        self.assertScores(expected, teams_data, arena_data)
+
+    def test_validate_error_invalid_notation(self):
+        teams_data = {
+            'ABC': {'zone': 0},
+            'DEF': {'zone': 1},
+            'GHI': {'zone': 2},
+        }
+        arena_data = {
+            0: { 'tokens': '' },
+            1: { 'tokens': 'Q' },
+            2: { 'tokens': '' },
+            3: { 'tokens': '' },
+            'other': { 'tokens': 'PGOY PGOY PGOY PGOY PGOY' },
+        }
+
+        self.assertValidationError(teams_data, arena_data)
+
+    def test_validate_error_not_enough_tokens(self):
+        teams_data = {
+            'ABC': {'zone': 0},
+            'DEF': {'zone': 1},
+            'GHI': {'zone': 2},
+        }
+        arena_data = {
+            0: { 'tokens': '' },
+            1: { 'tokens': '' },
+            2: { 'tokens': '' },
+            3: { 'tokens': '' },
+            'other': { 'tokens': 'PGOY PGOY PGOY PGOY' },
+        }
+
+        self.assertValidationError(teams_data, arena_data)
+
+    def test_validate_error_too_many_tokens(self):
+        teams_data = {
+            'ABC': {'zone': 0},
+            'DEF': {'zone': 1},
+            'GHI': {'zone': 2},
+        }
+        arena_data = {
+            0: { 'tokens': 'YYY YYY' },
+            1: { 'tokens': 'GGG GGG' },
+            2: { 'tokens': 'PPP PPP' },
+            3: { 'tokens': 'OOO OOO' },
+            'other': { 'tokens': '' },
+        }
+
+        self.assertValidationError(teams_data, arena_data)
+
+    def test_validate_error_invalid_combination_of_tokens(self):
+        teams_data = {
+            'ABC': {'zone': 0},
+            'DEF': {'zone': 1},
+            'GHI': {'zone': 2},
+        }
+        arena_data = {
+            0: { 'tokens': 'YYYYY' },
+            1: { 'tokens': 'YYYYY' },
+            2: { 'tokens': 'PPPPP' },
+            3: { 'tokens': 'OOOOO' },
+            'other': { 'tokens': '' },
+        }
+
+        self.assertValidationError(teams_data, arena_data)
+
+
 if __name__ == '__main__':
     unittest.main()
