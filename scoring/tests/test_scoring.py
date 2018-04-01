9e09d5e083f08d5f0dcb3b9f6795cff59a9341a4
Peter Law <PeterJCLaw@gmail.com>
2018-04-01 19:53:56 +0100

    Make this runnable

--- scoring/tests/test_scoring.py
+++ scoring/tests/test_scoring.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+
+import unittest
 
 # Path hackery
 import os.path
@@ -6,3 +9,7 @@ ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 sys.path.insert(0, ROOT)
 
 from score import Scorer, InvalidScoresheetException
+
+
+if __name__ == '__main__':
+    unittest.main()
